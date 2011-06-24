f0b8af1ebd49411323923f78f91695c8fca39de7
Francis Chong <francis@ignition.hk>
2011-06-24 15:41:24 +0800

    save the data every retry, do not fetch already retrieved data

--- lib/hk_geo/route_finder.rb
+++ lib/hk_geo/route_finder.rb
@@ -9,7 +9,6 @@ module HkGeo
       response = RestClient.get("http://maps.googleapis.com/maps/api/directions/json?origin=#{from}&destination=#{to}&sensor=false")
       routes = JSON(response.to_str)
       overview_polyline = routes["routes"].first["overview_polyline"]
-      puts overview_polyline.inspect
       GoogleMapsPolyline.decode_polyline(overview_polyline["points"], overview_polyline["levels"])
     end
   end
--- lib/tasks/route_finder.rb
+++ lib/tasks/route_finder.rb
@@ -1,27 +1,39 @@
 desc "find route"
 task :route do
   # process each line of data.csv
-  results = []
+  results = JSON(open("output/route.json").read) rescue {}
   csv = open("data/data.csv").read
 
   csv.each_line do |line|
     name, point1, coord1e, coord1n, point2, coord2e, coord2n, region, type = line.split(",")
 
-    # convert HK80
     begin
-      latlng1 = HkGeo::Converter.convert(coord1n, coord1e) rescue nil
-      latlng2 = HkGeo::Converter.convert(coord2n, coord2e) rescue nil
-      route = HkGeo::RouteFinder.route(latlng1.join(","), latlng2.join(",")) rescue nil
-      if latlng1 && latlng2 && route
-        results << {:name => name, :from => point1, :from_e => coord1e, :from_n => coord1n, :to => point2, :to_e => coord2e, :to_n => coord2n, :region => region, :type => type.strip, :route => route}
+      unless results[name]
+        # convert HK80
+        latlng1 = HkGeo::Converter.convert(coord1n, coord1e) rescue nil
+        latlng2 = HkGeo::Converter.convert(coord2n, coord2e) rescue nil
+
+        # find route
+        route = HkGeo::RouteFinder.route(latlng1.join(","), latlng2.join(",")) rescue nil
+      
+        # if everyting is OK, write
+        if latlng1 && latlng2 && route
+          results[name] = {:name => name, 
+              :from => point1, :from_e => coord1e, :from_n => coord1n, :from_converted => latlng1,
+              :to => point2, :to_e => coord2e, :to_n => coord2n, :to_converted => latlng2,
+              :region => region, :type => type.strip, :route => route,}
+          File.open("output/route.json", 'w') {|f| f.write(results.to_json) }
+        else
+          puts "ERROR: failed finding route #{name}"
+        end
+        sleep 1
       end
-      sleep 1
+
     rescue StandardError => e
-      puts "error: #{e.inspect}"
+      puts "ERROR: error loading route #{name}, #{e.inspect}"
       sleep 5
       retry
+
     end
   end
-
-  File.open("output/route.json", 'w') {|f| f.write(results.to_json) }
 end
